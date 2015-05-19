c41d08ec40fb152b2327ccf440fa36980482ac6d
Robert Falconer <inbox@robertfalconer.com>
2015-05-19 14:59:36 +0100

    Added KIF Test Spec template

---
+++ KIFTestSpec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,33 @@
+#import "KIFUITestActor+SKActions.h"
+
+// Uncomment or remove as necessary #import "SKPTestFactory.h"
+// Uncomment or remove as necessary #import "SKTestFactory.h"
+
+SKP_KIF_SPEC_BEGIN(<#NameOfSpec#>)
+
+describe(@"<#The user action that you are wanting to test e.g. when the user taps a stock item#>", ^{
+
+    context(@"<#A potential scenario that could affect the action e.g. the stock item has modifiers#>", ^{
+
+        it(@"<#The expected outcome in that scenario e.g. the stock item modifiers screen will show#>", ^{
+
+        });
+
+        it(@"<#Another expected outcome in that scenario e.g. the stock item gets added to the check with no modifiers#>", ^{
+
+        });
+    });
+
+    context(@"<#A potential scenario that could affect the action e.g. the stock item has no modifiers#>", ^{
+
+        it(@"<#The expected outcome in that scenario e.g. the stock item modifiers screen will not show#>", ^{
+
+        });
+
+        it(@"<#Another expected outcome in that scenario e.g. the stock item gets added to the check#>", ^{
+            
+        });
+    });
+});
+
+SKP_KIF_SPEC_END
